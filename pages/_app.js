bcc6c58c844487d9f56e1ae7c0a0717683aa1368
Jason Bennefield <benneja@tulsaschools.org>
2022-11-15 08:29:39 -0600

    adds a basic mui template taken from the mui templates page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
+function CourseCatalog({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default CourseCatalog
