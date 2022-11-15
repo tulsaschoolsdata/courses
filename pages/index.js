d3306222fca7d8a022ad58e41c496e6879c28136
Travis McClanahan <mcclatr@tulsaschools.org>
2022-11-15 10:47:19 -0600

    added getStaticProps to populate temporary fake data, WIP on UI

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import * as React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -16,11 +15,27 @@ import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import SchoolIcon from '@mui/icons-material/School';
 
+function LearnMore() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      <Link
+        color="inherit"
+        href="https://www.tulsaschools.org/about"
+        target="_blank"
+      >Click here</Link> to learn more about TPS.
+    </Typography>
+  );
+}
+
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://courses.tulsaschools.org/">
+      <Link
+        color="inherit"
+        href="https://courses.tulsaschools.org/"
+        target="_blank"
+      >
         Tulsa Public Schools
       </Link>{' '}
       {new Date().getFullYear()}
@@ -29,11 +44,9 @@ function Copyright() {
   );
 }
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 const theme = createTheme();
 
-export default function Album() {
+export default function Courses({ courses }) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -46,92 +59,97 @@ export default function Album() {
         </Toolbar>
       </AppBar>
       <main>
-        {/* Hero unit */}
         <Box
           sx={{
             bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
+            pt: 2,
+            pb: 2
           }}
         >
           <Container maxWidth="sm">
             <Typography
-              component="h1"
-              variant="h2"
+              component="h4"
+              variant="h4"
               align="center"
               color="text.primary"
-              gutterBottom
             >
               Courses
             </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
-            </Stack>
           </Container>
         </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
+        <Container maxWidth="md">
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+            {courses.map((course) => (
+              <Grid item key={course.number} xs={12} sm={6} md={4}>
                 <Card
                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                 >
                   <CardMedia
                     component="img"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random"
+                    image={course.url}
                     alt="random"
+                    height={50}
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      Heading
+                      {course.name}
                     </Typography>
                     <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
+                      {course.description}
                     </Typography>
                   </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
                 </Card>
               </Grid>
             ))}
           </Grid>
         </Container>
       </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
+
+      <Box sx={{ bgcolor: 'background.paper', pt: 16, pb: 1 }} component="footer">
+        <Divider />
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+        >
+          <LearnMore />
         </Typography>
         <Typography
           variant="subtitle1"
           align="center"
           color="text.secondary"
-          component="p"
         >
-          Something here to give the footer a purpose!
+          <Copyright />
         </Typography>
-        <Copyright />
       </Box>
-      {/* End footer */}
     </ThemeProvider>
   );
+}
+
+Courses.propTypes = {
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      number: PropTypes.number.isRequired,
+      url: PropTypes.string.isRequired
+    })
+  )
+}
+
+export async function getStaticProps() {
+  const courses = [...Array(10).keys()].map(n => (
+    {
+      name: `Course ${n}`,
+      description: `Course ${n} description goes here.`,
+      number: n,
+      url: 'https://source.unsplash.com/random'
+    }
+  ))
+
+  return {
+    props: {
+      courses
+    }
+  }
 }
