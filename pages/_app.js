84753d58f6252d3fb744a11d871c62bb36a6730e
Travis McClanahan <33558504+mcclatr@users.noreply.github.com>
2022-11-29 16:23:51 -0600

    Modify Page Head (#39)
    * add logo and title to page head
    * add head with course name to [id].js
    * Prettified Code!

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import Layout from '../components/layout'
+import Head from 'next/head'
+import React from 'react'
 
 export default function CourseCatalog({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <React.Fragment>
+      <Layout>
+        <Head>
+          <title>Course Catalog - Tulsa Public Schools</title>
+          <meta name="description" content="Created by the TPS Dev Team" />
+          <link rel="icon" href="/images/tps-logo-color.svg" />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </React.Fragment>
   )
 }
