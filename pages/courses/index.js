6d3dcade3f065719d4a3b6529fa826ef4063166d
Jason Bennefield <benneja@tulsaschools.org>
2023-01-10 19:38:37 -0600

    ensure courses comes from props

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Grid from '@mui/material/Grid'
 import CourseCard from '/components/courseCard'
-import { courses, schoolsGroupByCategory } from '/lib/models'
+import { courses as allCourses, schoolsGroupByCategory } from '/lib/models'
 import { courseShape } from '/lib/prop-types'
 import HeaderWithRecordCount from '/components/HeaderWithRecordCount'
 import Head from 'next/head'
@@ -40,6 +40,7 @@ Courses.propTypes = {
 
 export async function getStaticProps() {
   const schools = Object.entries(schoolsGroupByCategory)
+  const courses = allCourses
 
   return {
     props: {
