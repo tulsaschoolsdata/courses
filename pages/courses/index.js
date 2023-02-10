648144484de18d949377580198cf6c46cc6944e4
jasonben <benneja@tulsaschools.org>
2023-02-10 17:30:53 -0600

    infinite scroll feature to address mobile performance issues (#106)
    * infinite scroll feature to address mobile performance issues

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import { React, useState } from 'react'
 import PropTypes from 'prop-types'
 import Grid from '@mui/material/Grid'
 import CourseCard from '/components/courseCard'
-import { courses as allCourses, schoolsGroupByCategory } from '/lib/models'
+import { courses } from '/lib/models'
 import { courseShape } from '/lib/prop-types'
 import HeaderWithRecordCount from '/components/HeaderWithRecordCount'
 import Head from 'next/head'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
-export default function Courses({ courses }) {
+export default function Courses({ allCourses }) {
   const MetaTags = () => (
     <Head>
       <title>Courses offered by Tulsa Public Schools</title>
@@ -18,18 +19,45 @@ export default function Courses({ courses }) {
     </Head>
   )
 
+  const perPage = 100
+  const [courses, setCourses] = useState(allCourses.slice(0, perPage))
+  const [scrollPosition, setScrollPosition] = useState(perPage)
+  const [hasMore, setHasMore] = useState(true)
+
+  const fetchData = () => {
+    const increaseTotalSlice = scrollPosition + perPage
+    if (increaseTotalSlice >= allCourses.length) {
+      setHasMore(false)
+    } else {
+      setScrollPosition(increaseTotalSlice)
+    }
+    setCourses(() => allCourses.slice(0, increaseTotalSlice))
+  }
+
   return (
     <>
       <MetaTags />
-      <HeaderWithRecordCount title="Courses" records={courses.length} />
+      <HeaderWithRecordCount title="Courses" records={allCourses.length} />
 
-      <Grid container spacing={2} data-test="allCourses">
-        {courses.map((course) => (
-          <Grid key={course.course_number} item xs={12} sm={6}>
-            <CourseCard course={course} />
-          </Grid>
-        ))}
-      </Grid>
+      <InfiniteScroll
+        dataLength={courses.length}
+        next={fetchData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <Grid container spacing={2} data-test="allCourses">
+          {courses.map((course) => (
+            <Grid key={course.course_number} item xs={12} sm={6}>
+              <CourseCard course={course} />
+            </Grid>
+          ))}
+        </Grid>
+      </InfiniteScroll>
     </>
   )
 }
@@ -39,13 +67,11 @@ Courses.propTypes = {
 }
 
 export async function getStaticProps() {
-  const schools = Object.entries(schoolsGroupByCategory)
-  const courses = allCourses
+  const allCourses = courses
 
   return {
     props: {
-      courses,
-      schools,
+      allCourses,
     },
   }
 }
