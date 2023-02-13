c47ed9e180d0f30932545a6a43d2b78a31918822
Jason Bennefield <benneja@tulsaschools.org>
2023-02-13 16:19:11 -0600

    clean up leftovers

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react'
+import { React } from 'react'
 import PropTypes from 'prop-types'
 import { courses } from '/lib/models'
 import { courseShape } from '/lib/prop-types'
@@ -16,21 +16,6 @@ export default function Courses({ allCourses }) {
       />
     </Head>
   )
-
-  const perPage = 100
-  const [courses, setCourses] = useState(allCourses.slice(0, perPage))
-  const [scrollPosition, setScrollPosition] = useState(perPage)
-  const [hasMore, setHasMore] = useState(true)
-
-  const fetchData = () => {
-    const increaseTotalSlice = scrollPosition + perPage
-    if (increaseTotalSlice >= allCourses.length) {
-      setHasMore(false)
-    } else {
-      setScrollPosition(increaseTotalSlice)
-    }
-    setCourses(() => allCourses.slice(0, increaseTotalSlice))
-  }
 
   return (
     <>
