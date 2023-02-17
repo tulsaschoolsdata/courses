79482e4601c3f5e3c4a310fbeb2a439ac0f21b5c
mcclatr <mcclatr@users.noreply.github.com>
2023-02-17 17:13:15 +0000

    Prettified Code!

--- pages/search/results.js
+++ pages/search/results.js
@@ -18,15 +18,16 @@ const MetaTags = () => (
   </Head>
 )
 
-const courseNumbersToArray = (courseNumbers) => (
+const courseNumbersToArray = (courseNumbers) =>
   courseNumbers?.split(/\s+|,|\|/).filter((val) => val.match(/\d+/))
-)
 
 const queryParamsToFilters = (queryParams, courses) => {
   const filtersSearch = coerceIntoString(queryParams.search)
   const filtersCreditType = coerceIntoString(queryParams.creditType)
   const filtersSchools = coerceIntoArray(queryParams.schools)
-  const filtersCourseNumbers = coerceIntoArray(courseNumbersToArray(queryParams.courseNumbers))
+  const filtersCourseNumbers = coerceIntoArray(
+    courseNumbersToArray(queryParams.courseNumbers)
+  )
 
   let output = []
 
