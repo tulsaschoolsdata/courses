2cc3b38b9778088cb5add13d7d37c76b5828716e
Jason Bennefield <benneja@tulsaschools.org>
2023-02-17 14:00:49 -0600

    fixes tests and moves logic from search/results to models

--- cypress/e2e/search-results.cy.js
+++ cypress/e2e/search-results.cy.js
@@ -19,7 +19,7 @@ describe('search results', () => {
   })
 
   it('accepts courseNumbers as a query param', () => {
-    cy.visit('/search/results?courseNumbers=30444&courseNumbers=30274')
+    cy.visit('/search/results?courseNumbers=30444%2C30274')
     searchResults().should('contain.text', 'CAREER EXP')
     searchResults().should('contain.text', 'DIGITAL')
   })
--- pages/search/results.js
+++ pages/search/results.js
@@ -1,12 +1,12 @@
 import { React, useEffect, useState } from 'react'
-import Fuse from 'fuse.js'
-import { courses as allCourses } from '/lib/models'
 import HeaderWithRecordCount from '/components/HeaderWithRecordCount'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import InfiniteScrollCourses from '/components/InfiniteScrollCourses'
-import { isEmpty } from 'lodash'
-import { coerceIntoArray, coerceIntoString } from '/lib/utils'
+import {
+  courses as allCourses,
+  searchByQueryParams,
+} from '/lib/models'
 
 const MetaTags = () => (
   <Head>
@@ -18,67 +18,6 @@ const MetaTags = () => (
   </Head>
 )
 
-const courseNumbersToArray = (courseNumbers) =>
-  courseNumbers?.split(/\s+|,|\|/).filter((val) => val.match(/\d+/))
-
-const queryParamsToFilters = (queryParams, courses) => {
-  const filtersSearch = coerceIntoString(queryParams.search)
-  const filtersCreditType = coerceIntoString(queryParams.creditType)
-  const filtersSchools = coerceIntoArray(queryParams.schools)
-  const filtersCourseNumbers = coerceIntoArray(
-    courseNumbersToArray(queryParams.courseNumbers)
-  )
-
-  let output = []
-
-  const hasFilters =
-    [
-      filtersSearch,
-      filtersSchools,
-      filtersCreditType,
-      filtersCourseNumbers,
-    ].filter((n) => n.length > 0).length > 0
-
-  if (hasFilters) {
-    output = courses
-  }
-
-  if (!isEmpty(filtersCourseNumbers)) {
-    output = output.filter((course) =>
-      filtersCourseNumbers.includes(course.course_number)
-    )
-  }
-
-  if (!isEmpty(filtersCreditType)) {
-    output = output.filter((course) =>
-      course.credit_types.includes(filtersCreditType)
-    )
-  }
-
-  if (!isEmpty(filtersSchools)) {
-    output = output.filter(
-      (course) =>
-        course.school_numbers.filter((id) => filtersSchools.includes(id))
-          .length > 0
-    )
-  }
-
-  if (!isEmpty(filtersSearch)) {
-    const options = {
-      keys: [
-        { name: 'name', weight: 75 },
-        { name: 'description', weight: 5 },
-      ],
-      useExtendedSearch: true,
-      threshold: 0.2,
-    }
-    const fuse = new Fuse(output, options)
-    const fuseResults = fuse.search(filtersSearch)
-    output = fuseResults.map((result) => result.item)
-  }
-
-  return output
-}
 
 export default function SearchResults({ courses }) {
   const [searchResults, setSearchResults] = useState([])
@@ -87,7 +26,8 @@ export default function SearchResults({ courses }) {
   const queryParams = router.query
 
   useEffect(() => {
-    setSearchResults(queryParamsToFilters(queryParams, courses))
+    const results = searchByQueryParams(queryParams, courses)
+    setSearchResults(results)
   }, [queryParams, courses])
 
   return (
